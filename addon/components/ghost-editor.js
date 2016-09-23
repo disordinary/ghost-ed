855b2b39444cda814d9d3fa6e819f92e6b55a6f0
Ryan McCarvill <ryanmccarvill@Ryans-MBP.telecom>
2016-09-23 23:17:33 +1200

    renamed ghost-ed to ghost-editor, added onFirstChange event

--- addon/components/ghost-editor.js
+++ addon/components/ghost-editor.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import layout from '../templates/components/ghost-ed';
+import layout from '../templates/components/ghost-editor';
 import Mobiledoc from 'mobiledoc-kit';
 import { MOBILEDOC_VERSION } from 'mobiledoc-kit/renderers/mobiledoc';
 
@@ -34,17 +34,23 @@ export default Ember.Component.extend({
             markups : [],
             atoms : this.get('atoms') || [],
             spellcheck: true,
-            autofocus: false
+            autofocus: this.get('shouldFocusEditor')
         };
+
+        console.log(this.get('shouldFocusEditor'));
 
         let editor = this.editor = new Mobiledoc.Editor(options);
         
         editor.postDidChange(()=>{
-            
             Ember.run.join(() => {
                 //store a cache of the local doc so that we don't need to reinitialise it.
                 this._cached_doc = editor.serialize(MOBILEDOC_VERSION); 
                 this.sendAction('onChange', this._cached_doc);
+                if(this._cached_doc !== BLANK_DOC && !this._firstChange) {
+                    this._firstChange = true;
+                    this.sendAction('onFirstChange', this._cached_doc);
+                    //editor.selectRange(editor.post.headPosition());
+                }
             });
         });
         window.editor = editor; //make editor a global so that I can inspect it's state with the console.
@@ -56,6 +62,21 @@ export default Ember.Component.extend({
         }
         this.editor.render(this.$('.editor')[0]);
         this._rendered = true;
+        /*if(this.get('container').lookup('controller:application').currentPath === 'editor.edit') {
+
+        }*/
+
+        // shouldFocusEditor is only true when transitionaing from new to edit, otherwise it's false or undefined.
+        // therefore, if it's true it's after the first lot of content is entered and we expect the caret to be at the
+        // end of the document.
+        if(this.get('shouldFocusEditor')) {
+            var range = document.createRange();
+            range.selectNodeContents(this.editor.element);
+            range.collapse(false);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        }
     }
     
 });
